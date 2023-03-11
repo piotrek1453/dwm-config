16a6012fc70b80666ffc46e5f0f18984a43c7b1c
Piotr Jucha <piotrek1453@gmail.com>
2023-03-11 18:46:11 +0100

    extrabar

--- config.h
+++ config.h
@@ -5,6 +5,8 @@ static unsigned int borderpx  = 1;        /* border pixel of windows */
 static unsigned int snap      = 32;       /* snap pixel */
 static int showbar            = 1;        /* 0 means no bar */
 static int topbar             = 1;        /* 0 means bottom bar */
+static const int extrabarright      = 0;        /* 1 means extra bar text on right */
+static const char statussep         = ';';      /* separator between status bars */
 static char font[]            = "monospace:size=10";
 static char dmenufont[]       = "monospace:size=10";
 static const char *fonts[]          = { font };
